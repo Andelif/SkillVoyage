c9e48c1d676b5db49db69b7d46430ed02ece5a60
SA-Fiha <fihaahmed@outlook.com>
2024-09-20 06:09:40 +0600

    responsive pt1

--- frontend/src/Home page/HomeContent.js
+++ frontend/src/Home page/HomeContent.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import './HomeContent.css';
 import './Navbar';
-import Homebg from '../assets/bg1.png';
 import Homebg_dark from '../assets/Homebg_dark.png';
 
-const HomeContent = ({theme,setTheme}) => {
-        const toggle_mode = () => {
-          theme === 'light' ? setTheme('dark') : setTheme('light');
-        }
-      
+const HomeContent = () => {
   return (
     <div className='Homepage'>
-        <img src={theme === 'light' ? Homebg_dark: Homebg_dark} className='Homebg'/>
-        <button className="overlay-button" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+      <img src={Homebg_dark} className='Homebg' alt="Background" />
+      <button className="overlay-button">
         Get Started
       </button>
     </div>
-  )
+  );
 }
 
 export default HomeContent;
