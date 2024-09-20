73b8a98a583ac4fba00c49ae394d117abed7142f
SA-Fiha <fihaahmed@outlook.com>
2024-09-21 02:04:06 +0600

    small fix

--- frontend/src/Home page/HomeContent.js
+++ frontend/src/Home page/HomeContent.js
@@ -14,47 +14,56 @@
 // export default HomeContent;
 import React from 'react';
 import './HomeContent.css';
-import './Navbar';
 
 const HomeContent = () => {
   return (
     <div className='Homepage'>
-      <div className="banner-container">
-        <div className="text-container">
-          <div className="text-content">
+      <div className='banner-container'>
+        <div className='text-container'>
+          <div className='text-content'>
             <h1>
-              Taking <span className="highlight highlight-student">student</span> experience to the <span className="highlight highlight-next">next level</span>
+              Taking <span className="highlight-student">student</span> experience to the <span className="highlight-next">next level</span>
             </h1>
-            <p className="subtext">Transform your immigrant career journey into a guided, skill-building adventure</p>
-            <button className="progress-button">Check your progress</button>
+            <p className='subtext'>
+              Transform your immigrant career journey into a guided, skill-building adventure
+            </p>
+            <button className='progress-button'>Check your progress</button>
           </div>
-          <div className="vertical-line"></div> {/* Moved to the middle */}
+          <div className='vertical-line'></div>
         </div>
-        <div className="graphic-container">
-          <div className="arches">
-            <div className="arch arch1"></div>
-            <div className="arch arch2"></div>
-            <div className="arch arch3"></div>
+
+        <div className='graphic-container'>
+          <div className='arches'>
+            <div className='arch arch1'></div>
+            <div className='arch arch2'></div>
+            <div className='arch arch3'></div>
           </div>
-          <div className="dots">
-            {/* Randomly placed dots */}
-            <div className="dot dot1"></div>
-            <div className="dot dot2"></div>
-            <div className="dot dot3"></div>
-            <div className="dot dot4"></div>
-            <div className="dot dot5"></div>
-            <div className="dot dot6"></div>
-            {/* Add more dots */}
-            <div className="dot dot7"></div>
-            <div className="dot dot8"></div>
-            <div className="dot dot9"></div>
-            <div className="dot dot10"></div>
-            <div className="dot dot11"></div>
+          <div className='dots'>
+            <div className='dot dot1'></div>
+            <div className='dot dot2'></div>
+            <div className='dot dot3'></div>
+            <div className='dot dot4'></div>
+            <div className='dot dot5'></div>
+            <div className='dot dot6'></div>
+            <div className='dot dot7'></div>
+            <div className='dot dot8'></div>
+            <div className='dot dot9'></div>
           </div>
         </div>
+  
+      </div>
+
+          {/* Footer Section */}
+          <div className="footer">
+        <p>Credits: Created by Dash</p>
+        <p>Connect with us: 
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"> Facebook</a>, 
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"> Twitter</a>, 
+          <a href="mailto:contact@company.com"> Email</a>
+        </p>
       </div>
     </div>
   );
-};
+}
 
 export default HomeContent;
